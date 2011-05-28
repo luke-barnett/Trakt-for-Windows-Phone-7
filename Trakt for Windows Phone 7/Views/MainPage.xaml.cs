06654ca32aeab4690b06b78424f735a112b7092f
Luke Barnett <l@en.vc>
2011-05-29 10:27:47 +1200

    Small workaround for progress bars coming back from the dead

--- Trakt for Windows Phone 7/Views/MainPage.xaml.cs
+++ Trakt for Windows Phone 7/Views/MainPage.xaml.cs
@@ -13,19 +13,33 @@
 
         private int showCount;
         private int movieCount;
+        public bool moviesLoaded = false;
+        public bool showsLoaded = false;
 
         private void showImage_Opened(object sender, System.Windows.RoutedEventArgs e)
         {
             showCount++;
             if (showCount > 2)
+            {
                 showsProgressBar.IsIndeterminate = false;
+                showsLoaded = true;
+            }
         }
 
         private void movieImage_Opened(object sender, System.Windows.RoutedEventArgs e)
         {
             movieCount++;
             if (movieCount > 2)
+            {
                 moviesProgressBar.IsIndeterminate = false;
+                moviesLoaded = true;
+            }
+        }
+
+        private void toAnotherPage(object sender, System.EventArgs e)
+        {
+            moviesProgressBar.IsIndeterminate = false;
+            showsProgressBar.IsIndeterminate = false;
         }
     }
 }
