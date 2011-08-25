b0c6fcda5b8b96bdb69e53879e041bc5cdd54b14
Luke Barnett <l@en.vc>
2011-08-25 15:47:35 +1200

    Started work on initial loading page
    Signed-off-by: Luke Barnett <l@en.vc>

--- Trakt for Windows Phone 7/ViewModels/BaseViewModel.cs
+++ Trakt for Windows Phone 7/ViewModels/BaseViewModel.cs
@@ -1,21 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO.IsolatedStorage;
 using Caliburn.Micro;
 using System.Windows;
 using TraktAPI;
 using Microsoft.Phone.Reactive;
-using TraktAPI.TraktModels;
 using NetworkInterface = System.Net.NetworkInformation.NetworkInterface;
 
 namespace Trakt_for_Windows_Phone_7.ViewModels
 {
     public class BaseViewModel : Screen
     {
-        private IsolatedStorageSettings _userSettings;
-        private bool _progressBarVisible;
+        #region Private Parameters
 
+        private readonly IsolatedStorageSettings _userSettings;
+        private int _progressBarVisible;
+        private bool _firedLoadingEvent;
+        
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Event handler for finished loading event
+        /// </summary>
+        /// <param name="sender">The sender of the event</param>
+        /// <param name="eventArgs">The event arguments</param>
+        public delegate void FinishedLoadingHandler(object sender, FinishedLoadingEventArgs eventArgs);
+
+        /// <summary>
+        /// Event fired only once initial loading has been completed
+        /// </summary>
+        public event FinishedLoadingHandler FinishedLoading = delegate {};
+
+        #endregion
+
+        /// <summary>
+        /// The base view model that handles common tasks
+        /// </summary>
         public BaseViewModel()
         {
             _userSettings = IsolatedStorageSettings.ApplicationSettings;
@@ -25,23 +47,49 @@ namespace Trakt_for_Windows_Phone_7.ViewModels
             TryLogIn();
         }
 
+        #region Public Parameters
+
+        /// <summary>
+        /// Returns the application's name
+        /// </summary>
         public string ApplicationName { get { return "Trakt 7"; } }
 
+        /// <summary>
+        /// Returns the Font Family to use
+        /// </summary>
         public string FontFamily { get { return "/Trakt for Windows Phone 7;component/Fonts/Fonts.zip#Droid Sans"; } }
 
-        public bool ProgressBarVisible { get { return _progressBarVisible; } set { _progressBarVisible = value; NotifyOfPropertyChange(() => ProgressBarVisible); NotifyOfPropertyChange(() => ProgressBarVisibility); } }
+        /// <summary>
+        /// Whether or not to show the progress bar
+        /// </summary>
+        public bool ProgressBarVisible { get { return (_progressBarVisible > 0); } set { IncrementProgressBarVisibility(value); NotifyOfPropertyChange(() => ProgressBarVisible); NotifyOfPropertyChange(() => ProgressBarVisibility); } }
 
-        public string ProgressBarVisibility { get { return ProgressBarVisible ? "Visible" : "Collapsed"; } }
+        /// <summary>
+        /// The visibility state for the progress bar
+        /// </summary>
+        public Visibility ProgressBarVisibility { get { return ProgressBarVisible ? Visibility.Visible : Visibility.Collapsed; } }
 
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Records an error to Debug
+        /// </summary>
+        /// <param name="error">The error to record</param>
         public void HandleError(Exception error)
         {
             Debug.WriteLine(string.Format("CAUGHT ERROR {0}", error.Message));
         }
 
+        /// <summary>
+        /// Checks if there is an internet connection available
+        /// </summary>
+        /// <returns>The status of the internet connection</returns>
         public bool InternetConnectionAvailable()
         {
             var available = NetworkInterface.GetIsNetworkAvailable();
-
+            Debug.WriteLine("Internet connection {0} available",(available)?"is":"is not");
             if (!available)
             {
                 MessageBox.Show("No internet connection is available.  Try again later.", "Internet Required", MessageBoxButton.OK);
@@ -50,8 +98,12 @@ namespace Trakt_for_Windows_Phone_7.ViewModels
             return true;
         }
 
+        /// <summary>
+        /// Loads in the users settings from isolated storage
+        /// </summary>
         public void LoadSettings()
         {
+            Debug.WriteLine("Loading Settings");
             if (_userSettings.Contains("TraktUsername"))
                 TraktSettings.Username = _userSettings["TraktUsername"] as string;
             else
@@ -61,19 +113,42 @@ namespace Trakt_for_Windows_Phone_7.ViewModels
                 TraktSettings.Username = _userSettings["TraktPassword"] as string;
             else
                 TraktSettings.Username = String.Empty;
+            Debug.WriteLine("Loaded Values {0}", TraktSettings.ToString);
         }
 
+        /// <summary>
+        /// Saves the users settings from isolated storage
+        /// </summary>
+        public void SaveSettings()
+        {
+            _userSettings["TraktUsername"] = TraktSettings.Username;
+            _userSettings["TraktPassword"] = TraktSettings.Password;
+            _userSettings.Save();
+            Debug.WriteLine("Saved Settings");
+        }
+
+        /// <summary>
+        /// Attempts to log on to Trakt
+        /// </summary>
         public void TryLogIn()
         {
+            Debug.WriteLine("Try to log in");
             if(!String.IsNullOrEmpty(TraktSettings.Password) && !String.IsNullOrEmpty(TraktSettings.Username))
             {
-                TraktAPI.TraktAPI.TestAccount(TraktSettings.Username, TraktSettings.Password).Subscribe(onNext: response => UpdateLogInSettings(true), onError: error => UpdateLogInSettings(false));
+                TraktAPI.TraktAPI.TestAccount(TraktSettings.Username, TraktSettings.Password).Subscribe(response => UpdateLogInSettings(true), error => UpdateLogInSettings(false));
             }
             else
                 UpdateLogInSettings(false);
         }
 
+        #endregion
 
+        #region Private Methods
+
+        /// <summary>
+        /// Updates the log in status
+        /// </summary>
+        /// <param name="successfulLogIn">The status to update to</param>
         private void UpdateLogInSettings(bool successfulLogIn)
         {
             if (successfulLogIn)
@@ -84,6 +159,43 @@ namespace Trakt_for_Windows_Phone_7.ViewModels
                 TraktSettings.Password = String.Empty;
             }
             ProgressBarVisible = false;
+            Debug.WriteLine("{0} to log in",(successfulLogIn)?"Managed":"Failed");
+            OnFinishedLoading(new FinishedLoadingEventArgs(successfulLogIn));
         }
+
+        /// <summary>
+        /// Incremenets the progress bar visibility count
+        /// </summary>
+        /// <param name="increase">To increase or decrease</param>
+        private void IncrementProgressBarVisibility(bool increase)
+        {
+            _progressBarVisible = (increase) ? _progressBarVisible + 1 : _progressBarVisible - 1;
+            if (_progressBarVisible < 0)
+                _progressBarVisible = 0;
+        }
+
+        /// <summary>
+        /// Event trigger for when loading has finished
+        /// </summary>
+        protected void OnFinishedLoading(FinishedLoadingEventArgs args)
+        {
+            if (FinishedLoading == null || _firedLoadingEvent) return;
+            Debug.WriteLine("Finished Loading, firing event");
+            _firedLoadingEvent = true;
+            FinishedLoading(this, args);
+        }
+
+        #endregion
+    }
+
+    public class FinishedLoadingEventArgs : EventArgs
+    {
+        public readonly bool StatusOfAccount;
+
+        public FinishedLoadingEventArgs(bool statusOfAccount)
+        {
+            StatusOfAccount = statusOfAccount;
+        }
+    
     }
 }
--- Trakt for Windows Phone 7/ViewModels/MainPageViewModel.cs
+++ Trakt for Windows Phone 7/ViewModels/MainPageViewModel.cs
@@ -1,16 +1,134 @@
-﻿using Caliburn.Micro;
-using System.IO.IsolatedStorage;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using Caliburn.Micro;
+using Microsoft.Phone.Reactive;
+using Microsoft.Phone.Controls;
+using TraktAPI.TraktModels;
 
 namespace Trakt_for_Windows_Phone_7.ViewModels
 {
     public class MainPageViewModel : BaseViewModel
     {
+        #region Private Parameters
+
         private readonly INavigationService _navigationService;
+        private List<PivotItem> _pivotItems;
 
-
+        #endregion
+        
         public MainPageViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
+            _pivotItems = new List<PivotItem>();
+            StartLoading();
         }
+        #region Public Parameters
+
+        public List<PivotItem> PivotItems { get { return _pivotItems; } set { _pivotItems = value; NotifyOfPropertyChange(()=> PivotItems); }}
+
+        #endregion
+
+        #region Private Methods
+
+        private void StartLoading()
+        {
+            Debug.WriteLine("Starting loading of trending items");
+            GetTrendingMovies();
+        }
+
+        private void GetTrendingMovies()
+        {
+            Debug.WriteLine("Getting trending movies");
+            ProgressBarVisible = true;
+            TraktAPI.TraktAPI.GetTrendingMovies().Subscribe(UpdateDisplayForMovies, HandleError);
+        }
+
+        private void UpdateDisplayForMovies(TraktMovie[] trendingMovies)
+        {
+            Debug.WriteLine("Received trending movies, processing");
+            PivotItems.Clear();
+            for (int i = 0; i < trendingMovies.Length; i++)
+            {
+                var trendingMovie = trendingMovies[i];
+                var pivotMovie = new PivotItem();
+
+                var movieGrid = new Grid();
+                var gestureListener = GestureService.GetGestureListener(movieGrid);
+                gestureListener.DoubleTap += (sender, args) => MovieSelected(trendingMovie);
+
+                var image = new Image { Source = new BitmapImage(new Uri(trendingMovie.Images.Poster)) };
+
+                movieGrid.Children.Add(image);
+
+                pivotMovie.Content = movieGrid;
+                PivotItems.Add(pivotMovie);
+                Debug.WriteLine("Added trending movie {0}", trendingMovie.TitleAndYear);
+            }
+            PivotItems = new List<PivotItem>(PivotItems);
+            ProgressBarVisible = false;
+            Debug.WriteLine("Finished processing");
+        }
+
+        private void MovieSelected(TraktMovie selectedMovie)
+        {
+            Debug.WriteLine("Captured a double tap event on {0}", selectedMovie.TitleAndYear);
+        }
+
+        private void GetTrendingShows()
+        {
+            Debug.WriteLine("Getting trending shows");
+            ProgressBarVisible = true;
+            TraktAPI.TraktAPI.GetTrendingShows().Subscribe(UpdateDisplayForShows, HandleError);
+        }
+
+        private void UpdateDisplayForShows(TraktShow[] trendingShows)
+        {
+            Debug.WriteLine("Received trending shows, processing");
+            PivotItems.Clear();
+            for(int i = 0; i < trendingShows.Length; i++)
+            {
+                var trendingShow = trendingShows[i];
+                var pivotShow = new PivotItem();
+
+                var showGrid = new Grid();
+                var gestureListener = GestureService.GetGestureListener(showGrid);
+                gestureListener.DoubleTap += (sender, args) => ShowSelected(trendingShow);
+
+                var image = new Image { Source = new BitmapImage(new Uri(trendingShow.Images.Poster)) };
+
+                showGrid.Children.Add(image);
+
+                pivotShow.Content = showGrid;
+                PivotItems.Add(pivotShow);
+                Debug.WriteLine("Added trending show {0}", trendingShow.TitleAndYear);
+            }
+            PivotItems = new List<PivotItem>(PivotItems);
+            ProgressBarVisible = false;
+            Debug.WriteLine("Finished processing");
+        }
+
+        private void ShowSelected(TraktShow selectedShow)
+        {
+            Debug.WriteLine("Captured a double tap event on {0}", selectedShow.TitleAndYear);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void BtnGetTrendingMovies()
+        {
+            GetTrendingMovies();
+        }
+
+        public void BtnGetTrendingShows()
+        {
+            GetTrendingShows();
+        }
+
+        #endregion
     }
 }
